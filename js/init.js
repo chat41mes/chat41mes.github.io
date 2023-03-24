af476c1bace51bf4338849ced2a20c7f30fecdad
chat41mes <base41mes@gmail.com>
2023-03-23 23:55:21 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAzCq-iUohVGzf1u_sTry8eKOTs8sOfOW0",
+    authDomain: "chat41mes1.firebaseapp.com",
+    projectId: "chat41mes1",
+    storageBucket: "chat41mes1.appspot.com",
+    messagingSenderId: "600510137429",
+    appId: "1:600510137429:web:f8071f3f01765440bbc0dc",
+    measurementId: "G-SJ0LN3B7PX"
+    });
